6cd25304517629eee9b249ea34562e66345ac3fa
Pankaj <pk.thapa66@gmail.com>
2019-01-14 21:57:50 +0530

    BST *Insert(BST *, int) function added.
    BST *Insert(BST *, int): It adds nodes to a binary search tree.

--- BST.cpp
+++ BST.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 class BST{
     int data;
+    BST *left, *right;
 public:
-    BST *prev, *next;
-    BST() : data(0), prev(NULL), next(NULL){}
+    BST() : data(0), left(NULL), right(NULL){}
     BST(int n){
         data = n;
-        prev = next = NULL;
+        left = right = NULL;
     }
     void PreOrder(BST *);
     void InOrder(BST *);
@@ -16,16 +16,32 @@ public:
     void CreateMirror(BST *);
     void PrintLevelOrder(BST *);//!! BFS traversal.
     void PrintBFS(BST *, int);//!! BFS printing during traversing.
+	BST *Insert(BST *, int);
 };
 //-----------
+BST *BST :: Insert(BST *node, int value)
+{
+	if(!node){
+		return new BST(value);
+	}
+
+	if(value < node->data){
+		node->left = Insert(node->left, value);
+	}
+	else{
+		node->right = Insert(node->right, value);
+	}
+	return node;
+}
+
 void BST :: PreOrder(BST *node){
     if(!node){
         return;
     }
 
 	cout<<node->data<<" ";
-	PreOrder(node->prev);
-	PreOrder(node->next);
+	PreOrder(node->left);
+	PreOrder(node->right);
 }
 
 void BST :: PostOrder(BST *node){
@@ -33,8 +49,8 @@ void BST :: PostOrder(BST *node){
         return;
     }
 
-	PostOrder(node->prev);
-	PostOrder(node->next);
+	PostOrder(node->left);
+	PostOrder(node->right);
 	cout<<node->data<<" ";
 }
 
@@ -43,9 +59,9 @@ void BST :: InOrder(BST *node){
         return;
     }
 
-	InOrder(node->prev);
+	InOrder(node->left);
 	cout<<node->data<<" ";
-	InOrder(node->next);
+	InOrder(node->right);
 }
 
 int BST :: MaxDepth(BST *node){
@@ -53,19 +69,19 @@ int BST :: MaxDepth(BST *node){
         return 0;
     }
     int ldepth = 0, rdepth = 0;
-    return ((((ldepth = MaxDepth(node->prev)) > (rdepth = MaxDepth(node->next))) ? (ldepth + 1) : (rdepth + 1)));
+    return ((((ldepth = MaxDepth(node->left)) > (rdepth = MaxDepth(node->right))) ? (ldepth + 1) : (rdepth + 1)));
 }
 
 void BST :: CreateMirror(BST *node){
     if(!node){
         return;
     }
-    BST *temp = node->prev;
-    node->prev = node->next;
-    node->next = temp;
+    BST *temp = node->left;
+    node->left = node->right;
+    node->right = temp;
 
-    CreateMirror(node->prev);
-    CreateMirror(node->next);
+    CreateMirror(node->left);
+    CreateMirror(node->right);
 }
 
 void BST :: PrintLevelOrder(BST *node){
@@ -85,24 +101,33 @@ void BST :: PrintBFS(BST *node, int level){
         return;
     }
     else if(level > 1){
-        PrintBFS(node->next, level - 1);
-        PrintBFS(node->prev, level - 1);
+        PrintBFS(node->right, level - 1);
+        PrintBFS(node->left, level - 1);
     }
 }
 
 int main()
 {
     BST *root = NULL, o;
-    root = new BST(20);
-    root->next = new BST(30);
-    root->prev = new BST(10);
-    root->prev->prev = new BST(9);
-    root->prev->next = new BST(14);
-    root->prev->next->prev = new BST(11);
-    root->prev->next->next = new BST(16);
-    root->prev->next->next->prev = new BST(15);
-    root->next->prev = new BST(21);
-    root->next->next = new BST(40);
+    /*root = new BST(20);
+    root->right = new BST(30);
+    root->left = new BST(10);
+    root->left->left = new BST(9);
+    root->left->right = new BST(14);
+    root->left->right->left = new BST(11);
+    root->left->right->right = new BST(16);
+    root->left->right->right->left = new BST(15);
+    root->right->left = new BST(21);
+    root->right->right = new BST(40);*/
+	root = o.Insert(root, 60);
+	o.Insert(root, 10);
+	o.Insert(root, 5);
+	o.Insert(root, 30);
+	o.Insert(root, 40);
+	o.Insert(root, 70);
+	o.Insert(root, 25);
+	o.Insert(root, 23);
+	o.Insert(root, 88);
 
     cout<<"Pre-order traversal: ";
     o.PreOrder(root);
